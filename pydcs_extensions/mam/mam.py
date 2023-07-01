21e25be7df960c468a2a994cb6834dd327c8cd48
Hornet2041 <55866858+Hornet2041@users.noreply.github.com>
2023-07-01 14:58:24 -0400

    Update mam.py

--- pydcs_extensions/mam/mam.py
+++ pydcs_extensions/mam/mam.py
@@ -15,7 +15,7 @@ class M142_HIMARS_GLSDB(unittype.VehicleType):
     air_weapon_dist = 150000
     eplrs = True
 
-
+@vehiclemod
 class M142_HIMARS_ATACMS(unittype.VehicleType):
     id = "M142_HIMARS_ATACMS"
     name = "[CH] M142 HIMARS (ATACMS)"
@@ -24,7 +24,7 @@ class M142_HIMARS_ATACMS(unittype.VehicleType):
     air_weapon_dist = 296000
     eplrs = True
 
-
+@vehiclemod
 class M142_HIMARS_GMLRS(unittype.VehicleType):
     id = "M142_HIMARS_GMLRS"
     name = "M142 HIMARS-GMLRS"
@@ -33,7 +33,7 @@ class M142_HIMARS_GMLRS(unittype.VehicleType):
     air_weapon_dist = 92000
     eplrs = True
 
-
+@vehiclemod
 class M142_HIMARS_PRSM(unittype.VehicleType):
     id = "M142_HIMARS_PRSM"
     name = "[CH] M142 HIMARS (PrSM)"
@@ -42,7 +42,7 @@ class M142_HIMARS_PRSM(unittype.VehicleType):
     air_weapon_dist = 500000
     eplrs = True
 
-
+@vehiclemod
 class M142_HIMARS_PRSM_ASHM(unittype.VehicleType):
     id = "M142_HIMARS_PRSM_ASHM"
     name = "[CH] M142 HIMARS (PrSM AShM)"
@@ -51,7 +51,7 @@ class M142_HIMARS_PRSM_ASHM(unittype.VehicleType):
     air_weapon_dist = 500000
     eplrs = True
 
-
+@vehiclemod
 class CH_M270A1_GLSDB(unittype.VehicleType):
     id = "CH_M270A1_GLSDB"
     name = "[CH] M270A1 MLRS (GLSDB)"
@@ -60,7 +60,7 @@ class CH_M270A1_GLSDB(unittype.VehicleType):
     air_weapon_dist = 150000
     eplrs = True
 
-
+@vehiclemod
 class CH_M270A1_ATACMS(unittype.VehicleType):
     id = "CH_M270A1_ATACMS"
     name = "[CH] M270A1 MLRS (ATACMS)"
@@ -69,7 +69,7 @@ class CH_M270A1_ATACMS(unittype.VehicleType):
     air_weapon_dist = 296000
     eplrs = True
 
-
+@vehiclemod
 class CH_M270A1_GMLRS(unittype.VehicleType):
     id = "CH_M270A1_GMLRS"
     name = "[CH] M270A1 MLRS (GMLRS)"
