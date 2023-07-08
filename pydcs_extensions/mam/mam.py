d56340a7ca31fdc66c6bb07a2ace26551f68fdaa
Hornet2041 <55866858+Hornet2041@users.noreply.github.com>
2023-07-08 08:27:21 -0400

    Update mam.py

--- pydcs_extensions/mam/mam.py
+++ pydcs_extensions/mam/mam.py
@@ -121,3 +121,256 @@ class MonolitB(unittype.VehicleType):
     detection_range = 800000
     threat_range = 0
     air_weapon_dist = 0
+
+@vehiclemod
+class CH_PZH2000(unittype.VehicleType):
+    id = "CH_PZH2000"
+    name = "[CH] PzH 2000 SPG"
+    detection_range = 0
+    threat_range = 50000
+    air_weapon_dist = 50000
+    eplrs = True
+
+@vehiclemod
+class CH_Shahed136(unittype.VehicleType):
+    id = "CH_Shahed136"
+    name = "[CH] Shahed 136 LM"
+    detection_range = 0
+    threat_range = 360000
+    air_weapon_dist = 360000
+    eplrs = True
+
+@vehiclemod
+class CH_Alligator_Sniper(unittype.VehicleType):
+    id = "CH_Alligator_Sniper"
+    name = "[CH] Alligator Sniper AMR"
+    detection_range = 5000
+    threat_range = 3000
+    air_weapon_dist = 3000
+    eplrs = True
+
+@vehiclemod
+class CH_DSHK_HMG_RUS(unittype.VehicleType):
+    id = "CH_DSHK_HMG_RUS"
+    name = "[CH] DShK HMG"
+    detection_range = 5000
+    threat_range = 1800
+    air_weapon_dist = 1800
+    eplrs = True
+
+@vehiclemod
+class CH_DSHK_HMG_UKR(unittype.VehicleType):
+    id = "CH_DSHK_HMG_UKR"
+    name = "[CH] DShK HMG"
+    detection_range = 5000
+    threat_range = 1800
+    air_weapon_dist = 1800
+    eplrs = True
+
+@vehiclemod
+class CH_Stugna_P(unittype.VehicleType):
+    id = "CH_Stugna_P"
+    name = "[CH] Stugna-P ATGM"
+    detection_range = 5500
+    threat_range = 5500
+    air_weapon_dist = 5500
+    eplrs = True
+
+@vehiclemod
+class X_2S38(unittype.VehicleType):
+    id = "2S38"
+    name = "SPAAG 2S38"
+    detection_range = 12000
+    threat_range = 8000
+    air_weapon_dist = 8000
+    eplrs = True
+
+@vehiclemod
+class CH_S350_50P6_9M96D(unittype.VehicleType):
+    id = "CH_S350_50P6_9M96D"
+    name = "[CH] S-350 50P6 9M96D LN"
+    detection_range = 0
+    threat_range = 150000
+    air_weapon_dist = 150000
+    eplrs = True
+
+@vehiclemod
+class CH_S350_50P6_9M100(unittype.VehicleType):
+    id = "CH_S350_50P6_9M100"
+    name = "[CH] S-350 50P6 9M100 LN"
+    detection_range = 0
+    threat_range = 15000
+    air_weapon_dist = 15000
+    eplrs = True
+
+@vehiclemod
+class CH_S350_50N6(unittype.VehicleType):
+    id = "CH_S350_50N6"
+    name = "[CH] S-350 50N6 STR"
+    detection_range = 200000
+    threat_range = 0
+    air_weapon_dist = 0
+    eplrs = True
+
+@vehiclemod
+class CH_S350_50K6(unittype.VehicleType):
+    id = "CH_S350_50K6"
+    name = "[CH] S-350 50K6 CP"
+    detection_range = 0
+    threat_range = 0
+    air_weapon_dist = 0
+    eplrs = True
+
+@vehiclemod
+class CH_S350_96L6(unittype.VehicleType):
+    id = "CH_S350_96L6"
+    name = "[CH] S-350 96L6 SR"
+    detection_range = 300000
+    threat_range = 0
+    air_weapon_dist = 0
+
+@vehiclemod
+class CH_Centurion_C_RAM(unittype.VehicleType):
+    id = "CH_Centurion_C_RAM"
+    name = "[CH] Centurion C-RAM"
+    detection_range = 20000
+    threat_range = 2000
+    air_weapon_dist = 2000
+    eplrs = True
+
+@vehiclemod
+class MIM104_M903_PAC2(unittype.VehicleType):
+    id = "MIM104_M903_PAC2"
+    name = "[CH] SAM MIM-104 M903 PAC-2 GEM/T LN"
+    detection_range = 0
+    threat_range = 150000
+    air_weapon_dist = 150000
+    eplrs = True
+
+@vehiclemod
+class MIM104_M903_PAC3(unittype.VehicleType):
+    id = "MIM104_M903_PAC3"
+    name = "[CH] SAM MIM-104 M903 PAC-3 MSE LN"
+    detection_range = 0
+    threat_range = 120000
+    air_weapon_dist = 120000
+    eplrs = True
+
+@vehiclemod
+class MIM104_ECS(unittype.VehicleType):
+    id = "MIM104_ECS"
+    name = "[CH] SAM MIM-104 ECS"
+    detection_range = 0
+    threat_range = 0
+    air_weapon_dist = 0
+    eplrs = True
+
+@vehiclemod
+class MIM104_ANMPQ65(unittype.VehicleType):
+    id = "MIM104_ANMPQ65"
+    name = "[CH] SAM MIM-104 AN/MPQ-65 STR"
+    detection_range = 200000
+    threat_range = 0
+    air_weapon_dist = 0
+    eplrs = True
+
+@vehiclemod
+class MIM104_ANMPQ65A(unittype.VehicleType):
+    id = "MIM104_ANMPQ65A"
+    name = "[CH] SAM MIM-104 AN/MPQ-65A STR"
+    detection_range = 260000
+    threat_range = 0
+    air_weapon_dist = 0
+    eplrs = True
+
+@vehiclemod
+class MIM104_EPP(unittype.VehicleType):
+    id = "MIM104_EPP"
+    name = "[CH] SAM MIM-104 EPP"
+    detection_range = 0
+    threat_range = 0
+    air_weapon_dist = 0
+    eplrs = True
+
+@vehiclemod
+class CH_NASAMS3_LN_AMRAAM_ER(unittype.VehicleType):
+    id = "CH_NASAMS3_LN_AMRAAM_ER"
+    name = "[CH] NASAMS 3 LCHR LN (AMRAAM-ER)"
+    detection_range = 0
+    threat_range = 50000
+    air_weapon_dist = 50000
+    eplrs = True
+
+@vehiclemod
+class CH_NASAMS3_LN_AIM9X2(unittype.VehicleType):
+    id = "CH_NASAMS3_LN_AIM9X2"
+    name = "[CH] NASAMS 3 LCHR LN (AIM-9X-2)"
+    detection_range = 0
+    threat_range = 20000
+    air_weapon_dist = 20000
+    eplrs = True
+
+@vehiclemod
+class CH_NASAMS3_SR(unittype.VehicleType):
+    id = "CH_NASAMS3_SR"
+    name = "[CH] NASAMS 3 SR"
+    detection_range = 120000
+    threat_range = 0
+    air_weapon_dist = 0
+    eplrs = True
+
+@vehiclemod
+class CH_NASAMS3_CP(unittype.VehicleType):
+    id = "CH_NASAMS3_CP"
+    name = "[CH] NASAMS 3 CP"
+    detection_range = 0
+    threat_range = 0
+    air_weapon_dist = 0
+    eplrs = True
+
+@vehiclemod
+class PGL_625(unittype.VehicleType):
+    id = "PGL_625"
+    name = "SPAAGM PGL-625"
+    detection_range = 20000
+    threat_range = 10000
+    air_weapon_dist = 10000
+    eplrs = True
+
+@vehiclemod
+class PantsirS1(unittype.VehicleType):
+    id = "PantsirS1"
+    name = "SPAAGM Pantsir-S1"
+    detection_range = 36000
+    threat_range = 20000
+    air_weapon_dist = 20000
+    eplrs = True
+
+@vehiclemod
+class PantsirS2(unittype.VehicleType):
+    id = "PantsirS2"
+    name = "SPAAGM Pantsir-S2"
+    detection_range = 40000
+    threat_range = 30000
+    air_weapon_dist = 30000
+    eplrs = True
+
+@vehiclemod
+class ZBD04A(unittype.VehicleType):
+    id = "ZBD04A"
+    name = "ZBD-04A"
+    detection_range = 0
+    threat_range = 4800
+    air_weapon_dist = 0
+    eplrs = True
+
+@vehiclemod
+class CH_T90M(unittype.VehicleType):
+    id = "CH_T90M"
+    name = "[CH] T-90M MBT"
+    detection_range = 8000
+    threat_range = 5000
+    air_weapon_dist = 5000
+    eplrs = True
+
+
